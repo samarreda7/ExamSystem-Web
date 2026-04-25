6ab270797aa49864dbc763ee83fd83451a177382
samarreda7 <samarreda804@gmail.com>
2026-04-25 21:51:55 +0300

    feat: add initial auth and layout components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {
+     path: '',
+     component: AuthLayoutComponent,
+     children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+   },
+    {
+     path: '',
+     component: MainLayoutComponent,
+    },
+];
