dd5b3e9df70a3b33bb836c3152a42065d242a60b
samarreda7 <samarreda804@gmail.com>
2026-05-02 16:34:09 +0300

    feat(auth-ui): build login and nested register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
+import { StudentRegisterComponent } from './features/register/student-register/student-register.component';
+import { TeacherRegisterComponent } from './features/register/teacher-register/teacher-register.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { NotfoundComponent } from './features/notfound/notfound.component';
 
@@ -12,7 +14,15 @@ export const routes: Routes = [
      component: AuthLayoutComponent,
      children: [
       { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
+      {
+        path: 'register',
+        component: RegisterComponent,
+        children: [
+          { path: '', redirectTo: 'student', pathMatch: 'full' },
+          { path: 'student', component: StudentRegisterComponent },
+          { path: 'teacher', component: TeacherRegisterComponent },
+        ],
+      },
     ],
    },
     {
---
+++ src/app/features/register/student-register/student-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-register',
+  imports: [],
+  templateUrl: './student-register.component.html',
+  styleUrl: './student-register.component.css',
+})
+export class StudentRegisterComponent {
+
+}
