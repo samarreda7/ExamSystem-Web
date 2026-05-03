537157531218838bc5c398767978b2c8ab0feb5d
samarreda7 <samarreda804@gmail.com>
2026-05-04 00:49:46 +0300

    feat(layout): update main layout structure

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
