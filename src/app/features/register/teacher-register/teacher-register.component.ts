88bdcd32a55e4198343ad2e67ef76eb7b05a48fa
samarreda7 <samarreda804@gmail.com>
2026-05-04 00:50:04 +0300

    feat(teacher-register): update teacher registration form behavior

--- src/app/features/register/teacher-register/teacher-register.component.ts
+++ src/app/features/register/teacher-register/teacher-register.component.ts
@@ -1,27 +1,88 @@
-import { Component, inject, NgZone, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../../core/auth/services/auth.service';
 import { SubjectService } from '../../../core/auth/services/subject.service';
 import { Subject } from '../../../core/models/subject.interface';
+import { Teacher } from '../../../core/models/teacher.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-teacher-register',
+  imports: [ReactiveFormsModule],
   templateUrl: './teacher-register.component.html',
   styleUrl: './teacher-register.component.css',
 })
 export class TeacherRegisterComponent implements OnInit {
+  private readonly authService = inject(AuthService);
   private readonly subjectService = inject(SubjectService);
-
+  private readonly router = inject(Router);
   subjectlist: Subject[] = [];
+  registerSubscribe: Subscription = new Subscription();
+
+  registerform: FormGroup = new FormGroup(
+    {
+      firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\+?[0-9]{10,15}$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#!@\$%\^&*-]).{8,}$/),
+      ]),
+      rePassword: new FormControl('', Validators.required),
+      subjectId: new FormControl('', Validators.required),
+    },
+    { validators: [this.confirmpassword] },
+  );
 
   ngOnInit(): void {
     this.getAllSubjects();
   }
 
+  confirmpassword(group: AbstractControl) {
+    const password = group.get('password')?.value;
+    const rePassword = group.get('rePassword')?.value;
+    if (rePassword !== password && rePassword !== '') {
+      group.get('rePassword')?.setErrors({ mismatch: true });
+      return { mismatch: true };
+    }
+    return null;
+  }
+
   getAllSubjects() {
     this.subjectService.getAllSubjects().subscribe({
       next: (res: Subject[]) => {
-       this.subjectlist = res;
+        this.subjectlist = res;
       },
       error: (err) => console.log(err),
     });
+  }
+
+  onSubmit() {
+    if (this.registerform.valid) {
+      this.registerSubscribe.unsubscribe();
+      const teacherData = this.registerform.value as Teacher & { rePassword?: string };
+      delete teacherData.rePassword;
+      this.registerSubscribe = this.authService.signUpTeacher(teacherData).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(["/login"]);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
   }
 }
