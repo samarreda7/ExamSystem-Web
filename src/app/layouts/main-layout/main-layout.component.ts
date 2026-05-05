59094578faf0ba51484ba970c920a804671f1143
samarreda7 <samarreda804@gmail.com>
2026-05-05 12:47:10 +0300

    fix(layout): repair main layout router outlet setup

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterOutlet } from "../../../../node_modules/@angular/router/types/_router_module-chunk";
 import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  imports: [NavbarComponent, RouterOutlet]',
   imports: [NavbarComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
